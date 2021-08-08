b6e208611620cb6d476f3050fa3ae4f41b47a751
Constancegit status <constanceingram94@gmail.com>
2021-08-08 16:17:11 -0500

    added framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { AnimatePresence } from 'framer-motion'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} />
+      </AnimatePresence>
+
+  )
 }
 
 export default MyApp
