a2975125d5e7a3551218a2287439e37e91aaa0df
Constancegit status <constanceingram94@gmail.com>
2021-09-07 10:32:58 -0500

    trying to fix off kilter background image

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import { motion } from 'framer-motion'
+//import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    //<Layout>
       <motion.div id="background" 
       initial={{ opacity: 0}} 
       animate={{ opacity: 1 }} 
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       transition={{ duration: 0.2}}>
         <Component {...pageProps} />
       </motion.div>
+    //</Layout>
 
   )
 }
